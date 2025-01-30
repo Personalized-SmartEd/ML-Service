fad12504594a8c6b267d1400f53cfcf53a64525d
Gagan Mishra <simonsimple305@gmail.com>
2025-01-30 09:04:29 +0530

    specified port for deployment

--- App.py
+++ App.py
@@ -1,5 +1,10 @@
 import uvicorn
 from src.main import app
+from dotenv import load_dotenv
+import os  
+
+load_dotenv('.env')
+PORT = os.getenv('PORT')
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=PORT or 8000)
