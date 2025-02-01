b432b2ed3509fce85269a380583961bc7dc94b03
Gagan Mishra <simonsimple305@gmail.com>
2025-02-01 15:46:49 +0530

    Some docs and debugging.

--- src/Routers/assessment.py
+++ src/Routers/assessment.py
@@ -8,17 +8,18 @@ router = APIRouter(
     prefix="/assessment",
     tags=["Assessment"]
 )
-service = InitialAssessmentService() 
+static_service = InitialAssessmentService() 
+dynamic_service = DynamicAssessmentService()
 
 # Routes
 @router.get("/static", response_model=QuizResponseModel)
 async def get_initial_quiz():
-    return await service.generate_initial_quiz()
+    return await static_service.generate_initial_quiz()
 
 @router.post("/static", response_model=LearningStyleResult)
 async def get_initial_results(responses: QuizSubmission):
-    return await service.process_results(responses)
+    return await static_service.process_results(responses)
 
 @router.post("/dynamic", response_model=AssessmentResult)
 async def get_dynamic_assessment(scores_data: PastScoresModel):
-    return  service.calculate_performance(scores_data.subject, scores_data.scores)
+    return  dynamic_service.calculate_performance(scores_data.subject, scores_data.scores)
--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from src.Routers import assessment, quiz_bot, tutor_bot, doubt_bot
 
-app = FastAPI()
+app = FastAPI(title='SmartEd - ML Service', version='0.1.0')
 
 @app.get("/")
 async def health_check():
